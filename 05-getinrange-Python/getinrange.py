b53be298b50704b181c4a7a84620f1610c8d69a0
koushik maruvada <koushikmaruvada@koushiks-MacBook-Air.local>
2020-07-14 12:52:22 +0530

    Jul 14  12:52:22

--- 05-getinrange-Python/getinrange.py
+++ 05-getinrange-Python/getinrange.py
@@ -5,7 +5,7 @@
 # or if x is greater than the upper bound, return the upper bound.
 
 def fun_getinrange(x, bound1, bound2):
-        if(bound1>x>bound2):
+        if(bound1<x<bound2):
             return x
         elif(x>bound2):
             return bound2
