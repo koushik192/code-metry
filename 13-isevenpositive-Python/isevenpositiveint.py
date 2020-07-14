1f182ac9af6e367e37226a45656e6ec549c33772
koushik maruvada <koushikmaruvada@koushiks-MacBook-Air.local>
2020-07-14 21:34:17 +0530

    Jul 14  21:34:17

--- 13-isevenpositive-Python/isevenpositiveint.py
+++ 13-isevenpositive-Python/isevenpositiveint.py
@@ -5,7 +5,11 @@
 # than crashing), and isevenpositiveint(123456) returns True.
 
 def isevenpositiveint(x):
-        if(x%2==0)and(x>0):
+        if(isinstance(x,int)):
+            return True
+        elif(x==None):
+            return False
+        elif(x%2==0)& (x>0):
             return True
         else:
             return False
