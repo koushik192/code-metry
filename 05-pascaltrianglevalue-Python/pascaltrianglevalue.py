da9ae962d7e300c9af12b8169e8ed9e4658efe01
koushik maruvada <koushikmaruvada@koushiks-MacBook-Air.local>
2020-07-15 19:46:20 +0530

    Jul 15  19:46:20

--- 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
+++ 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
@@ -9,7 +9,7 @@
 
 import math
 def fun_pascaltrianglevalue(row, col):
-        if(row or col ==0):
+        if(row and col ==0):
             return None
         else:
-            return math.factorial(row)/(math.factorial(row-col)*math.factorial(col))
+            return int(math.factorial(row)/(math.factorial(row-col)*math.factorial(col)))
