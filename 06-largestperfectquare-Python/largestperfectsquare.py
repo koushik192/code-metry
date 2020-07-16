69237cbd2c1a9f89e4b27532aafc6ef655e98b4b
koushik maruvada <koushikmaruvada@koushiks-MacBook-Air.local>
2020-07-16 13:04:53 +0530

    Jul 16  13:04:53

--- 06-largestperfectquare-Python/largestperfectsquare.py
+++ 06-largestperfectquare-Python/largestperfectsquare.py
@@ -6,7 +6,8 @@
 # assert(largestPerfectSquare(26) == 25)
 # Hint: you may wish to use a similar approach to how you solved isPerfectSquare on the hw.
 # Another hint: This can be written using just one or two lines of Python.
-
 def largestperfectsquare(n):
-	# your code goes here
-	pass
+    for i in range(n):
+      print(i)
+
+largestperfectsquare(24)
