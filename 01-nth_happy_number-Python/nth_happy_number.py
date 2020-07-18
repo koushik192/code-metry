e81e7feba18ce68f9729c865072c62a49d99b7e1
koushik maruvada <koushikmaruvada@koushiks-MacBook-Air.local>
2020-07-18 10:03:33 +0530

    Jul 18  10:03:32

--- 01-nth_happy_number-Python/nth_happy_number.py
+++ 01-nth_happy_number-Python/nth_happy_number.py
@@ -14,13 +14,13 @@
 def fun_nth_happy_number(n):
     c=[]
     happy=[]
-    while(n<0):
+    while(n!=0):
         n=sum(int(i)**2 for i in str(n))
         if n in c:
             return False
         c.append(n)
     return True
-    for i in range(n):
+    for i in range(n,c):
         if fun_nth_happy_number(i):
             happy.append(i)
         print(happy)
