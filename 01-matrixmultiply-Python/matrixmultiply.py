30780a01a3aa40cc4799d54e148f9f704fdb54a4
koushik maruvada <koushikmaruvada@koushiks-MacBook-Air.local>
2020-07-18 15:13:45 +0530

    Jul 18  15:13:45

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -5,7 +5,16 @@
 
 
 def fun_matrixmultiply(m1, m2):
-    return None
+    try:
+        res=[]
+        for i in range(len(m1)):
+            l=[]
+            for j in range(len(m1[0])):
+                l.append(m1[i][j]+m2[i][j])
+            res.append(l)
+        return res
+    except:
+        return None
 
 
 
