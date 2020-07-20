fc15ff7ce834f55698b85cd2ea4a547b65526ca0
koushik maruvada <koushikmaruvada@koushiks-MacBook-Air.local>
2020-07-20 10:37:33 +0530

    Jul 20  10:37:33

--- 02-binarysearch-Python/binarysearch.py
+++ 02-binarysearch-Python/binarysearch.py
@@ -1,15 +1,28 @@
-"""You're going to write a binary search function.
-You should use an iterative approach - meaning
-using loops.
-Your function should take two inputs:
-a Python list to search through, and the value
-you're searching for.
-Assume the list only has distinct elements,
-meaning there are no repeated values, and 
-elements are in a strictly increasing order.
-Return the index of value, or -1 if the value
-doesn't exist in the list."""
+# """You're going to write a binary search function.
+# You should use an iterative approach - meaning
+# using loops.
+# Your function should take two inputs:
+# a Python list to search through, and the value
+# you're searching for.
+# Assume the list only has distinct elements,
+# meaning there are no repeated values, and 
+# elements are in a strictly increasing order.
+# Return the index of value, or -1 if the value
+# doesn't exist in the list."""
 
-def binary_search(input_array, value):
-    # Your code goes here
-    pass
+# def binary_search(input_array, value):
+#     low=0
+#     mid=0
+#     high=len(input_array)-1
+#     while(low<=high):
+#         mid=(high+low)/2
+#         if(input_array(mid)<value):
+#             low=mid+1
+#         elif(input_array(mid)>value):
+#             high=mid-1
+#         else:
+#             return mid
+#     return -1
+a=[1,2,3,4,5,6,7,8]
+b=len(a)
+print(b)
