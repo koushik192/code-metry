bb978912d6e0a6fc80fab3fd47c98702737b6acf
koushik maruvada <koushikmaruvada@koushiks-MacBook-Air.local>
2020-07-22 09:32:52 +0530

    kthoccurance

--- 03-kth_occurrences-Python/kth_occurrences.py
+++ 03-kth_occurrences-Python/kth_occurrences.py
@@ -5,6 +5,18 @@
 
 
 def fun_kth_occurrences(s, n):
-	return 'a'
+    s=s.strip()
+    if len(s)==1:
+        return s
+    else:
+        m=[]
+        for i in s:
+            if(s.count(i),i)not in m and i!='':
+                m.append((s.count(i),i)) 
+        m.sort(key=lambda x:x[0],reverse=True)
+        try:
+            return m[n-1][1]
+        except:
+            return m[0][1]
 
-
+fun_kth_occurrences("helllo woorld",1)
