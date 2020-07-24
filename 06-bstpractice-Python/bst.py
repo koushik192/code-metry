d6044cbd09ae9b2e747193d1041169d00fd1fd9d
koushik maruvada <koushikmaruvada@koushiks-MacBook-Air.local>
2020-07-24 12:52:38 +0530

    bstdone

--- 06-bstpractice-Python/bst.py
+++ 06-bstpractice-Python/bst.py
@@ -10,13 +10,58 @@ class BST(object):
 
     def insert(self, new_val):
         # Your code goes here
-        pass
+       return self.driveinsert(self.root,new_val)
+    
+    def driveinsert(self,start,new_val):
+        if (start):
+            if (new_val<start.value):
+                if (start.left):
+                    self.driveinsert(start.left,new_val)
+                else:
+                    start.left=Node(new_val)
+            elif(new_val>start.value):
+                if(start.right):
+                    self.driveinsert(start.right,new_val)
+                else: 
+                    start.right=Node(new_val)
+        
 
     def printSelf(self):
         # Your code goes here
+        res=""
+        traversal=self.preorderprint(self.root,[])
+        for i in traversal:
+            res=res+str(value)+"_"
+        res=res[::-1]
+        return res
+
+
         pass
+    def preorderprint(self,start,traversal):
+        if start:
+            traversal.append(start.value)
+            if start.left:
+                traversal=self.preorderprint(start.left,traversal)
+            if start.left:
+                traversal=self.preorderprint(start.right,traversal)
+        return traversal
+
+
         
     def search(self, find_val):
         # Your code goes here
+        return self.drivesearch(self.root,find_val)
         pass
-
+    
+    def drivesearch(self,start,find_val):
+        if type(find_val)!=type(1):
+            return False
+        else:
+            if start:
+                if (find_val==start.value):
+                    return True
+                elif(find_val>start.value):
+                    return self.drivesearch(start.right,find_val)
+                elif(find_val<start.value):
+                    return self.drivesearch(start.left,find_val)
+        return False 
