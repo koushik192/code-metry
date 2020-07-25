83034d619b60360c06bd419faeda51d1a0bf9b37
koushik maruvada <koushikmaruvada@koushiks-MacBook-Air.local>
2020-07-25 11:45:11 +0530

    Jul 25  11:45:11

--- 01-nth_tidynumber-Python/nth_tidynumber.py
+++ 01-nth_tidynumber-Python/nth_tidynumber.py
@@ -6,20 +6,28 @@
 # fun_nth_tidynumber(15) = 17
 # fun_nth_tidynumber(35) = 46
 def istidy(n):
-    prev =10
-    while(n):
-        rem=(n%10)
-        n=(n//10)
-        if (rem>prev):
-            return False
-        prev = rem
-    return True
+    if (n<10):
+        return True
+    else:
+
+        while(n!=0):
+            rem=(n%10)
+            n=(n/10)
+            prev=n%10
+            if (rem>prev):
+                return False
+            prev = rem
+        return True
 
 def fun_nth_tidynumber(n):
-    k=[]
-    for i in range(1000):
-        if istidy(i)==True:
-            k.append(i)
-    print(i)
+    c=0
+    i=1
+    while(True):
+        if istidy(i):
+            if (c==n):
+                break
+            c=c+1
+        i=i+1
+    return(i)
 
 fun_nth_tidynumber(0)
