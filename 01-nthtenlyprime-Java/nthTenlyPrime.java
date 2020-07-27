3d6ffb3dc4d30616355def39c799f5da73982fa0
koushik maruvada <koushikmaruvada@koushiks-MacBook-Air.local>
2020-07-27 16:43:25 +0530

    not required

---
+++ 01-nthtenlyprime-Java/nthTenlyPrime.java
@@ -0,0 +1,13 @@
+// nthTenlyPrime(n)
+// We will say that a number is “tenly” (a made-up term) if the digits of the 
+// number add up to 10. So 1153 is tenly, but 153 is not. With this in mind, write 
+// the function nthTenlyPrime(n) that takes a non-negative int n and returns the 
+// nth number that is both tenly and prime. You should also write all the required 
+// helper functions. The first several tenly primes are: 19, 37, 73, 109, 127…
+
+
+class nthtenlyprime {
+	public int fun_nthtenlyprime(int n){
+		return 1;
+	}
+}
