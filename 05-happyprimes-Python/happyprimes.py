5fc2f517017ee06f1d84fbd9dd8959de3b761dc0
koushik maruvada <koushikmaruvada@koushiks-MacBook-Air.local>
2020-07-27 12:37:25 +0530

    happy prime

--- 05-happyprimes-Python/happyprimes.py
+++ 05-happyprimes-Python/happyprimes.py
@@ -8,7 +8,45 @@
 # And that's top-down design! Here we go.... 
 # Note: the autograder will grade each of the following functions, so they are required. 
 # However, they also are here specifically because they are just the right helper 
-# functions to make nthHappyNumber(n) easier to write!
-def ishappyprimenumber(n):
+# functions to make nthHappyNumber(n) easier to writ
+def sumOfSquaresOfDigits(n):
+    return n**2
+
+
+def ishappynumber(n):
     # Your code goes here
+    if n<1:
+        return False
+    if n==1:
+        return True
+    while(n!=1):
+        if len(str(n))==1:
+            break
+        i=list(str(n))
+        i=list(map(int,i))
+        print(i)
+        i=list(map(sumOfSquaresOfDigits,i))
+        n=sum(i)
+    if n==1:
+        return True
+    else:
+        return False
     pass
+
+def isprime(n):
+    if n<2:
+        return False
+    else:
+        for i in range(2,(n//2)+1):
+            if n%i==0:
+                return False
+        return True
+
+def ishappyprimenumber(n):
+    if isprime(n) and ishappynumber(n):
+        return True
+    return False
+
+    pass
+
+ishappyprimenumber(833)
